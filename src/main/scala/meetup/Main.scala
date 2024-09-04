95e6eea0bf14b04362dfe06cec484809c26e1844
Thomas Alexandre <thomas.alexandre@external.wolt.com>
2024-09-04 10:59:42 +0200

    cleanup

--- src/main/scala/meetup/Main.scala
+++ src/main/scala/meetup/Main.scala
@@ -1,28 +1,16 @@
 package meetup
 
-import scala.scalajs.js
 import org.scalajs.dom
 import com.raquo.laminar.api.L.{*, given}
 
-def main(args: Array[String]): Unit = {
-  println("Hello stockholm!")
-
-  val nameVar = Var(initial = "world")
-  val app = {
-    div(
-      label(b("Your name: ")),
-      input(
-        placeholder := "Enter your name here",
-        onInput.mapToValue --> nameVar
-      ),
-      p(
-        "Hello, ",
-        child <-- nameVar.signal.map(_.toUpperCase)
-      )
-    )
-  }
-
+def main(args: Array[String]): Unit =
+  val nameVar = Var(initial = "")
+  val app = div(
+    label(b("Your name: ")),
+    input(
+      placeholder := "Enter your name ", onInput.mapToValue --> nameVar),
+    p("Hello, ", child <-- nameVar.signal.map(_.toUpperCase))
+  )
   render(dom.document.getElementById("app"), app)
-}
 
 
