d76dfd228d845c3cd127da2f5520f9c339429047
Thomas Alexandre <thomas.alexandre@external.wolt.com>
2024-09-05 14:21:28 +0200

    sample from official doc

--- src/main/scala/meetup/Main.scala
+++ src/main/scala/meetup/Main.scala
@@ -1,10 +1,38 @@
 package meetup
 
-import scala.scalajs.js
-import org.scalajs.dom
+import cats.effect.IO
+import tyrian.Html.*
+import tyrian.*
 
-@main def hello(): Unit =
-  dom.document.querySelector("#app").innerHTML =
-    """
-    <h1>Hello, Wolt Scala Meetup!</h1>
-    """
+import scala.scalajs.js.annotation.*
+
+@JSExportTopLevel("TyrianApp")
+object Main extends TyrianIOApp[Msg, Model]:
+
+  def router: Location => Msg = Routing.none(Msg.NoOp)
+
+  def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model.init, Cmd.None)
+
+  def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
+    case Msg.Increment => (model + 1, Cmd.None)
+    case Msg.Decrement => (model - 1, Cmd.None)
+    case Msg.NoOp      => (model, Cmd.None)
+
+  def view(model: Model): Html[Msg] =
+    div(button(onClick(Msg.Decrement))("-"),
+      div(model.toString),
+      button(onClick(Msg.Increment))("+"))
+
+  def subscriptions(model: Model): Sub[IO, Msg] = Sub.None
+
+opaque type Model = Int
+object Model:
+  def init: Model = 0
+
+  extension (i: Model)
+    def +(other: Int): Model = i + other
+    def -(other: Int): Model = i - other
+
+enum Msg:
+  case Increment, Decrement, NoOp
